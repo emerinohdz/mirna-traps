a34e1b9b10b7705f964c6108b9da57a1ba0ed74e
Edgar Merino <emerino@nuevebit.com>
2015-02-18 17:00:57 -0600

    database configuration done

--- miroculus-mrna-rest/src/test/java/ITDatabasePopulator.java
+++ miroculus-mrna-rest/src/test/java/ITDatabasePopulator.java
@@ -4,17 +4,29 @@
  * and open the template in the editor.
  */
 
+import com.nuevebit.miroculus.mrna.rest.DatabasePopulator;
+import javax.inject.Inject;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * @author emerino
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:app-context.xml")
 public class ITDatabasePopulator {
+
+    @Inject
+    private DatabasePopulator populator;
 
     public ITDatabasePopulator() {
     }
 
+    @Test
+    public void testPopulate() {
+        populator.populate();
+    }
 }
