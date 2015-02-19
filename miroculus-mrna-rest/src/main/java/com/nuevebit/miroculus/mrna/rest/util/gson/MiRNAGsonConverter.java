cc58b55445ed85311ef844f21e065abbdc7d33ca
Edgar Merino <emerino@nuevebit.com>
2015-02-18 22:47:54 -0600

    webservice rest finished

--- miroculus-mrna-rest/src/main/java/com/nuevebit/miroculus/mrna/rest/util/gson/MiRNAGsonConverter.java
+++ miroculus-mrna-rest/src/main/java/com/nuevebit/miroculus/mrna/rest/util/gson/MiRNAGsonConverter.java
@@ -8,6 +8,16 @@ package com.nuevebit.miroculus.mrna.rest.util.gson;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.nuevebit.miroculus.mrna.core.Author;
+import java.lang.reflect.Type;
 import org.restlet.ext.gson.GsonConverter;
 import org.restlet.ext.gson.GsonRepresentation;
 import org.restlet.representation.Representation;
@@ -25,7 +35,9 @@ public class MiRNAGsonConverter extends GsonConverter {
                 public boolean shouldSkipField(FieldAttributes f) {
                     String name = f.getName();
 
-                    return name.equals("version") || name.equals("id");
+                    return name.equals("version")
+                    || name.equals("id")
+                    || name.equals("publications");
                 }
 
                 @Override
@@ -61,6 +73,17 @@ public class MiRNAGsonConverter extends GsonConverter {
     private GsonBuilder configureBuilder(GsonBuilder builder) {
         builder.addDeserializationExclusionStrategy(exclusionStrat);
         builder.addSerializationExclusionStrategy(exclusionStrat);
+        builder.registerTypeAdapter(Author.class,
+                new JsonSerializer<Author>() {
+
+                    @Override
+                    public JsonElement serialize(Author src, 
+                            Type typeOfSrc, 
+                            JsonSerializationContext context) {
+
+                        return new JsonPrimitive(src.getName());
+                    }
+                });
         return builder;
     }
 
